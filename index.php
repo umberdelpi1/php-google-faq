ef73630302d24d0159a07aa34aa0ebae11eca9d5
Umberto Del Piano <umberdelpi1@gmail.com>
2020-05-06 16:47:06 +0200

    Faq inserita

--- index.php
+++ index.php
@@ -22,19 +22,27 @@
         Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.
         Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
       ],
+      [
+        'domanda' => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+        'risposta' => "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.",
+      ],
+      [
+        'domanda' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+        'risposta' => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Int",
+      ],
     ];
     ?>
   </head>
 
   <body>
-    <h1>
+  <div class="faq">
     <?php
       foreach ($faq as $intro) {
-        echo $intro["domanda"] . "<br>";
-        echo $intro["risposta"] . "<br> ------------- </br>";
+        echo '<h1>' . $intro["domanda"] . "</h1>" . "<br>";
+        echo '<p>' . $intro["risposta"] . "</p>" . "<br>";
       }
-      ?>
-   </h1>
+    ?>
+  </div>
 
   </body>
 </html>
